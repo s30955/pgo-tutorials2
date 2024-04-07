d44b92c66b45eabbe2dbe286af3350de1dc2f621
Barbara Nowosielska <s30955@pjwstk.edu.pl>
2024-04-07 18:52:59 +0200

    Skonczone zadania

--- cw2/src/StudentGroup.java
+++ cw2/src/StudentGroup.java
@@ -1,7 +1,17 @@
+import java.util.ArrayList;
+
 public class StudentGroup {
     public String nazwa;
 
+    int maksymalnaIloscStudentów=15;
+    ArrayList <Student> studentList = new ArrayList<Student>();
     public void dodajNowegoStudenta(){
+     if(studentList.contains(student)){
+         throw new IllegalStateException("Student już jest wpisany");
+     }
+    if(studentList.size()+1 > maksymalnaIloscStudentów){
+        throw new IllegalArgumentException("Liczba studentów jest za duża");
+    }
 
     }
 }
