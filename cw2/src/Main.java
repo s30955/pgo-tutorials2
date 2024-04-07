b7bba7cb6e1845684eaa7d0e0902575dd08197cc
Barbara Nowosielska <s30955@pjwstk.edu.pl>
2024-04-07 18:15:20 +0200

    Zrobione zadanie 1

---
+++ cw2/src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+    Student student = new Student();
+    student.grades=new double[] {2, 2.5, 3, 3.5, 4, 4.5, 5};
+
+    }
+    public static void obliczSrednia(){
+
+    }
+}
---
+++ cw2/src/Student.java
@@ -0,0 +1,24 @@
+public class Student {
+    public String fname;
+    public String lname;
+    public String indexNumber;
+    public String email;
+    public String Address;
+    public double[] grades;
+
+    public double obliczSrednia() {
+        double srednia = 0;
+
+        if (grades.length > 20) {
+            System.out.println("Za dużo ocen");
+        } else if (grades.length == 0) {
+            throw new IllegalArgumentException("Brak ocen");
+        } else if (grades.length < 0 && grades.length <= 20) {
+            for (int i = 0; i < grades.length; i++) {
+                srednia += grades[i];
+            }
+            srednia /= grades.length;
+        }
+    return srednia;
+    }
+}
